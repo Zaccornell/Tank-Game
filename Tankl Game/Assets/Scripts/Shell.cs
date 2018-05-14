4720a7ccfb1e05734bc07278dac2492c8533e89f
Zaccornell <39174603+Zaccornell@users.noreply.github.com>
2018-05-14 20:26:11 +1000

    First commit for Tank Game

---
+++ Tankl Game/Assets/Scripts/Shell.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+
+    public int damage = 20;
+
+    // Use this for initialization
+    void Start()
+    {
+        Destroy(this.gameObject, 4f);
+    }
+
+    void OnCollisionEnter(Collision Other)
+    {
+        if (Other.gameObject.tag == "Player")
+        {
+            Other.gameObject.GetComponent<TankController>().TakeDamage(damage);
+        }
+        Destroy(this.gameObject);
+    }
+}
+
+
---
+++ Tankl Game/Assets/Scripts/TankController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour {
+
+	public float forwardSpeed = 0.1f;
+	public float backwardSpeed = 0.08f;
+	public float rotationalSpeed = 2f;
+
+	public float turretRotationalSpeed = 3f;
+
+	public GameObject turret;
+
+    public float shellSpeed = 20;
+
+    public GameObject shellPrefab;
+
+    public Transform shellSpawnPoint;
+
+    public KeyCode forwardsKey = KeyCode.W;
+    public KeyCode backwardsKey = KeyCode.S;
+    public KeyCode rotateLeftKey = KeyCode.A;
+    public KeyCode rotateRightKey = KeyCode.D;
+    public KeyCode rotateTurretLeftKey = KeyCode.Q;
+    public KeyCode rotateTurretRightKey = KeyCode.E;
+    public KeyCode fireKey = KeyCode.Space;
+
+    public int health = 100;
+
+    public void TakeDamage(int damageToTake)
+    {
+        health = health - damageToTake;
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (rotateTurretLeftKey)) 
+		{
+			turret.transform.Rotate (0, -turretRotationalSpeed, 0, Space.Self);
+		}
+
+		if (Input.GetKey (rotateTurretRightKey))
+		{
+			turret.transform.Rotate (0, turretRotationalSpeed, 0, Space.Self);
+		}
+		
+		if (Input.GetKey (forwardsKey)) {
+			transform.position = transform.position + transform.forward * forwardSpeed;
+		}
+
+		if (Input.GetKey (backwardsKey)) {
+			transform.position = transform.position + transform.forward * -backwardSpeed;
+		}
+
+		if (Input.GetKey (rotateLeftKey)) {
+			transform.Rotate (transform.up * -rotationalSpeed);
+		}
+
+		if (Input.GetKey (rotateRightKey)) {
+			transform.Rotate (transform.up * rotationalSpeed);
+		}
+
+        if (Input.GetKey (fireKey))
+        {
+            GameObject Go = Instantiate(shellPrefab, shellSpawnPoint.position, Quaternion.identity) as GameObject;
+            Go.GetComponent<Rigidbody>().AddForce(turret.transform.forward * shellSpeed, ForceMode.Impulse);
+        }
+        if (health <= 0)
+        {
+            return;
+        }
+
+	}
+}
